a5f6c96aba1dd489eb2ee3f19c48f216a43db04d
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2022-08-08 14:19:37 +0200

    Adjust Docker setup
    * Remove `chown` commands since it depends on the system (Windows works out-of-the box)
    * Remove `sudo` since it was only used for the `chown` command
    * Use former code from `run_project.sh` inside `entrypoint.sh`
    * Directly mount the entrypoint script (do not copy it because then a change in the script would need a rebuild of the container)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
 
 #######
-# CHOWN MOUNTED FILES
+# LOAD ENVIRONMENT VARIABLES FOR DEVELOPMENT TOOLS
 #######
 
-echo "Run 'chown' on mounted files ..."
-sudo chown -R $USER:$USER ~/project
+echo "Load environment variables ..."
+
+# load Node variables
+source ~/.nvm/nvm.sh
 
 #######
-# RUN MOUNTED PROJECT SCRIPT
+# GO TO PROJECT DIRECTORY
 #######
 
-# the project script is not used as entrypoint script to avoid rebuilding the container each time the project
-# script is updated
-echo "Run project script ..."
-/bin/bash -c ~/run_project.sh
+echo "Go to ~/project ..."
+cd ~/project
+
+#######
+# ADJUST PROJECT SETTINGS
+#######
+
+echo "Adjust project settings ..."
+
+# substitute the backend URL in the target environment file
+# => ATTENTION: Use "|" as delimiter for sed because the URL contains the usual delimiter "/"
+#               and would not be recognized else
+
+BACKEND_LINE_REGEX="BACKEND_URL: '.*'"
+BACKEND_LINE_TARGET="BACKEND_URL: '${BACKEND_URL}'"
+sed -i "s|${BACKEND_LINE_REGEX}|${BACKEND_LINE_TARGET}|" ./src/environments/environment.${ANGULAR_ENVIRONMENT}.ts
+
+#######
+# STARTUP COMMAND
+#######
+
+# just keep the container alive; the application has to be started interactively
+echo "Keep container alive ..."
+sleep infinity
+
+# alternative to sleep infinity: automatically build and run application
+# => ATTENTION:
+#    - Use '--host 0.0.0.0' for allowing requests from the host
+#    - Use '--poll 2000' to automatically check for updated files every 2 seconds
+#echo "Start application ..."
+#npm install && ng serve --configuration=${ANGULAR_ENVIRONMENT} --host 0.0.0.0 --poll 2000
